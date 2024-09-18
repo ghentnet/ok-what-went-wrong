d04bb01ee9e02a98ceb69698e683c2e80908bb07
Michael Sean Becker <michael@ritual.com>
2024-09-17 23:13:08 -0700

    Update site for autopublish

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,34 @@
 const markdownIt = require("markdown-it")
+const { DateTime } = require('luxon')
+
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("md", function (content = "") {
     return markdownIt({ html: true }).render(content);
   })
+  
+  eleventyConfig.addLayoutAlias('page', 'layouts/page')
+  eleventyConfig.addLayoutAlias('post', 'layouts/post')
+  
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`)
+  eleventyConfig.addShortcode("appVer", () => `v${process.env.APP_VERSION || '0.0.0'}`)
+
+  eleventyConfig.addFilter('readableDate', dateObj => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'utc',
+    }).setLocale('en').toLocaleString(DateTime.DATE_FULL)
+  })
+
+  /* Creating a collection of blog posts by filtering based on folder and filetype */
+  eleventyConfig.addCollection('blog', (collectionApi) => {
+    const posts = collectionApi.getFilteredByGlob('./src/posts/*.md')
+    .filter(process.env.ELEVENTY_RUN_MODE !== "serve" ? item => !item.data.draft : item => item.data).reverse()
+    console.log(posts)
+    return posts
+  })
+
+
+  eleventyConfig.addGlobalData("env", process.env);
 
   return {
     dir: {
---
+++ src/index.md
@@ -0,0 +1,9 @@
+---
+layout: page
+title: Ok, What Went Wrong? A Home Lab Experiment
+---
+
+# Ok, What Went Wrong?
+## 103 Homelab Co-op
+
+{% include "./_includes/components/articleList.njk" %}
